c8f057424c1aa3ab0c6bedfeb076d49d4df8c15d
Kevin Schaal <kevin.schaal@m31coding.com>
2024-02-17 16:49:15 +0100

    remove test data from export

--- src/commons/index.ts
+++ src/commons/index.ts
@@ -1,4 +1,3 @@
 export { ArrayUtilities } from './array-utilities';
 export { HashUtilities } from './hash-utilities';
 export { StringUtilities } from './string-utilities';
-export { TestData } from './test-data';
