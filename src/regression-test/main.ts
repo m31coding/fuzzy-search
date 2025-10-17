b2ee929b50f737669b0dc54a27d7c0cb971e66eb
Kevin Schaal <kevin.schaal@m31coding.com>
2025-10-17 10:49:25 +0200

    fix: regression test

--- src/regression-test/main.ts
+++ src/regression-test/main.ts
@@ -10,16 +10,19 @@ import { Meta } from '../interfaces/meta.js';
 import { Query } from '../interfaces/query.js';
 import { SearcherFactory } from '../searcher-factory.js';
 
+const outputPath = './src/regression-test/output';
+const shortColumnWidth = 8;
+const wideColumnWidth = 40;
+const RLI = "\u2067", LRI = "\u2066", PDI = "\u2069";
+
+const text = readFileSync('./data/world-ctvs.txt', 'utf8');
+const lines = text.split('\n').slice(1);
+const entities = lines.map((l, index) => ({ id: index, name: l }));
+
 interface GeoEntity {
     id: number;
     name: string;
 }
-
-const outputPath = './src/regression-test/output';
-const outputColumnWidth = 40;
-const text = readFileSync('./data/world-ctvs.txt', 'utf8');
-const lines = text.split('\n').slice(1);
-const entities = lines.map((l, index) => ({ id: index, name: l }));
 
 const config = Config.createDefaultConfig();
 config.normalizerConfig.allowCharacter = (_) => true;
@@ -31,8 +34,7 @@ const indexingMeta: Meta = searcher.indexEntities(
     (e) => e.id,
     (e) => e.name.split(';')
 );
-
-writeFileSync(`${outputPath}/_indexing-meta.txt`, metaToJson(indexingMeta));
+writeFileSync(`${outputPath}/_indexing-meta.txt`, metaToJson(indexingMeta), { encoding: 'utf8' });
 
 console.log("Running queries...");
 
@@ -47,18 +49,18 @@ runQuery('tokyo', '東京都');
 runQuery('tokyo-prefix', '東京');
 runQuery('tbilisi', 'თბილისი');
 runQuery('tbilisi-deletion', 'თბიისი');
-runQuery('kuwait-city', 'مدينة الكويت');
-runQuery('kuwait-city-prefix', 'مدينة الك');
+runQuery('kuwait-city', 'مدينة الكويت', true);
+runQuery('kuwait-city-prefix', 'مدينة الك', true);
 
 console.log("Finished.")
 
-function runQuery(queryName: string, queryString: string) {
+function runQuery(queryName: string, queryString: string, rtl: boolean = false) {
     const query: Query = new Query(queryString);
     const result: EntityResult<GeoEntity> = searcher.getMatches(query);
     console.log(`'${queryString}' (${queryName}): ${result.matches.length} matches.`);
     const queryJson = JSON.stringify(query, null, 2);
     const metaJson = metaToJson(result.meta);
-    const matchesString = matchesToString(result.matches);
+    const matchesString = matchesToString(result.matches, rtl);
     const output = `${queryJson}\n\n${metaJson}\n\n${matchesString}`;
     writeFileSync(`${outputPath}/${queryName}.txt`, output);
 }
@@ -67,26 +69,44 @@ function metaToJson(meta: Meta): string {
     return JSON.stringify(Object.fromEntries(meta.allEntries), null, 2);
 }
 
-function matchesToString(matches: EntityMatch<GeoEntity>[]): string {
+function matchesToString(matches: EntityMatch<GeoEntity>[], rtl: boolean): string {
     const header =
-        padRight("Rank", 8) +
-        padRight("Entity", outputColumnWidth) +
-        padRight("Matched String", outputColumnWidth) +
-        padRight("Quality", 8) +
+        padRight("Rank", shortColumnWidth) +
+        padRight("Entity", wideColumnWidth) +
+        padRight("Matched String", wideColumnWidth) +
+        padRight("Quality", shortColumnWidth) +
         "\n\n";
-    const matchesString = matches.map((m, i) => matchToString(m, i + 1)).join('\n');
+    const matchesString = matches.map((m, i) => matchToString(m, i + 1, rtl)).join('\n');
     return header + matchesString;
 }
 
-function matchToString(match: EntityMatch<GeoEntity>, rank: number): string {
+function matchToString(match: EntityMatch<GeoEntity>, rank: number, rtl: boolean): string {
+    if (!rtl) {
+        return (
+            padRight(rank.toString(), shortColumnWidth) +
+            padRight(match.entity.name, wideColumnWidth) +
+            padRight(match.matchedString, wideColumnWidth) +
+            padRight(match.quality.toFixed(2), shortColumnWidth)
+        );
+    }
     return (
-        padRight(rank.toString(), 8) +
-        padRight(match.entity.name, outputColumnWidth) +
-        padRight(match.matchedString, outputColumnWidth) +
-        padRight(match.quality.toFixed(2), 8)
+        padAndMark(rank.toString(), shortColumnWidth, false) +
+        padAndMark(match.entity.name, wideColumnWidth, true) +
+        padAndMark(match.matchedString, wideColumnWidth, true) +
+        padAndMark(match.quality.toFixed(2), shortColumnWidth, false)
     );
+}
+
+function padAndMark(s: string, targetWidth: number, rtl: boolean): string {
+    const padded = rtl ? padLeft(s, targetWidth) : padRight(s, targetWidth);
+    const mark = rtl ? RLI : LRI;
+    return mark + padded + PDI;
 }
 
 function padRight(s: string, targetWidth: number): string {
     return s + ' '.repeat(Math.max(0, targetWidth - s.length));
+}
+
+function padLeft(s: string, targetWidth: number): string {
+    return ' '.repeat(Math.max(0, targetWidth - s.length)) + s;
 }
