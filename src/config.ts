6b2962735f158cfe8fcc9478f6e8919e5f67c15e
Kevin Schaal <kevin.schaal@m31coding.com>
2025-10-22 12:19:18 +0200

    feat: sort order for the config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import { FuzzySearchConfig } from './fuzzy-searchers/fuzzy-search-config.js';
 import { NormalizerConfig } from './normalization/normalizer-config.js';
+import { SortOrder } from './sort-order.js';
 import { SubstringSearchConfig } from './suffix-array-searchers/substring-search-config.js';
 
 /**
@@ -10,12 +11,14 @@ export class Config {
    * Creates a new instance of the Config class.
    * @param normalizerConfig The configuration for the default normalizer.
    * @param maxQueryLength The maximum query length.
+   * @param sortOrder The sort order for the entity matches.
    * @param fuzzySearchConfig The fuzzy search configuration.
    * @param substringSearchConfig The substring search configuration.
    */
   public constructor(
     public normalizerConfig: NormalizerConfig,
     public maxQueryLength: number,
+    public sortOrder: SortOrder,
     public fuzzySearchConfig: FuzzySearchConfig,
     public substringSearchConfig: SubstringSearchConfig
   ) { }
@@ -26,8 +29,10 @@ export class Config {
    */
   public static createDefaultConfig(): Config {
     const normalizerConfig = NormalizerConfig.createDefaultConfig();
+    const maxQueryLength = 150;
+    const sortOrder = SortOrder.QualityAndMatchedString;
     const fuzzySearchConfig = FuzzySearchConfig.createDefaultConfig();
     const substringSearchConfig = SubstringSearchConfig.createDefaultConfig();
-    return new Config(normalizerConfig, 150, fuzzySearchConfig, substringSearchConfig);
+    return new Config(normalizerConfig, maxQueryLength, sortOrder, fuzzySearchConfig, substringSearchConfig);
   }
 }
