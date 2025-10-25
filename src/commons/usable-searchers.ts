fe0a4d92fd153013774d397b6de09af9952d2513
Kevin Schaal <kevin.schaal@m31coding.com>
2025-10-25 16:30:38 +0200

    docs

--- src/commons/usable-searchers.ts
+++ src/commons/usable-searchers.ts
@@ -1,11 +1,21 @@
 import { SearcherSpec } from '../interfaces/searcher-spec.js';
 import { SearcherType } from '../interfaces/searcher-type.js';
 
-// todo: docs
+/**
+ * Holds the searchers that are usable based on the available and requested searchers.
+ */
 export class UsableSearchers {
 
+    /**
+     * The usable searchers mapped by their type.
+     */
     private readonly usableSearchers: Map<SearcherType, SearcherSpec>;
 
+    /**
+     * Creates a new instance of the UsableSearchers class.
+     * @param availableSearchers The types of searchers that are available.
+     * @param requestedSearchers The searchers requested for the query.
+     */
     public constructor(
         availableSearchers: SearcherType[],
         requestedSearchers: SearcherSpec[]) {
@@ -18,14 +28,29 @@ export class UsableSearchers {
         }
     }
 
+    /**
+     * Checks whether a searcher of the given type is usable.
+     * @param type The type of the searcher.
+     * @returns Whether the searcher is usable.
+     */
     public has(type: SearcherType): boolean {
         return this.usableSearchers.has(type);
     }
 
+    /**
+     * Gets the minimum quality for the given searcher type.
+     * @param type The type of the searcher.
+     * @returns The minimum quality for the searcher type.
+     */
     public minQuality(type: SearcherType): number {
         return this.usableSearchers.get(type)!.minQuality;
     }
 
+    /**
+     * Gets the searcher specification for the given type.
+     * @param type The type of the searcher.
+     * @returns The searcher specification for the searcher type.
+     */
     public spec(type: SearcherType): SearcherSpec {
         return this.usableSearchers.get(type)!;
     }
